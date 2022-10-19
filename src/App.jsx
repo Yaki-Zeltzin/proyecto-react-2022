096f264e2ef00e54f8c09a0498372c1290fa908f
Zeltzin Romero <zeltzin.rom96@gmail.com>
2022-10-19 11:16:47 -0500

    Se creó la lista de criptomonedas y se agrego Axios para la perición

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-function App() {
-  return (
-    <h1>HOLA MUNDO</h1>
-  )
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default App
+function App() {
+ 
+  const API_URL = 'https://api.coincap.io/v2/';
+
+  const [criptos, setCriptos] = useState();
+
+  useEffect(()=>{
+    axios.get(`${API_URL}assets`)
+    .then((data) =>{
+      setCriptos(data.data.data);
+    })
+    .catch(()=>{
+      console.error('La petición falló')
+    });
+  },[]);
+  
+
+  if(!criptos) return <span>Cargando...</span>
+
+  return (
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {
+          criptos.map(({id,name, priceUsd}) => (
+          <li key={id}>Nombre: {name} Precio: {priceUsd} </li>
+        ))
+         }
+      </ol>
+    </>
+  )
+};
+
+export default App;
